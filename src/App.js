67021edeaf327c420e823cdf76088e6e984bf554
1sisodiyaji <637golusingh@gmail.com>
2023-09-25 09:22:42 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from "./component/Header";
+import Blogs from "./component/Blogs";
+import Pagination from "./component/Pagination";
+import { useContext,useEffect } from "react";
+import{AppContext} from "./context/AppContext"
+
+
+
+export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
+  useEffect(() => {
+    fetchBlogPosts();
+  },[]);
+  return  (
+    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+      <Header/>
+      <Blogs/>
+      <Pagination/>
+    </div>
+  )
+}
